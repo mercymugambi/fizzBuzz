b2bcb1753a0b05856fe8bb8396513f4783ab477c
mercymugambi <mercymugambi2016@gmail.com>
2023-04-28 10:46:31 +0300

    Fix error

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -5,11 +5,13 @@ module.exports = function (){
   for (let i=1; i<=100; i+1){
     if 
     (i%3 ===0 && i%5 ===0){
-      output.push("fizzBuzz");
+      output.push("FizzBuzz");
     }else if(i%5 === 0){
       output.push("Buzz");
     }else if(i%3 === 0){
       output.push("Fizz");
+    }else{
+      output.push(i);
     }
     return output
   }
